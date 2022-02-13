0184b649541fcf93d7ce55cf11bd84aff1b55dbe
KojoBoat <egyirchristian07@gmail.com>
2022-02-13 11:36:04 +0000

    Implemented Business Layer for CatalogApi Service

---
+++ src/Services/Catalog/Catalog.Api/Repositories/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using Catalog.Api.Entities;
+
+namespace Catalog.Api.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Product> GetProductAsync(string Id);
+        Task<IEnumerable<Product>> GetProductByNameAsync(string Name);
+        Task<IEnumerable<Product>> GetProductByCategoryAsync(string Category);
+        Task CreateProduct(Product product);
+        Task<bool> UpdateProductAsync(Product product);
+        Task<bool> DeleteProductAsync(string Id);
+    }
+}
---
+++ src/Services/Catalog/Catalog.Api/Repositories/ProductRepository.cs
@@ -0,0 +1,62 @@
+﻿using Catalog.Api.Data;
+using Catalog.Api.Entities;
+using Catalog.Api.Exceptions;
+using MongoDB.Driver;
+
+namespace Catalog.Api.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ICatalogContext _context;
+
+        public ProductRepository(ICatalogContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsAsync()
+        {
+            return await _context.Products.Find(p => true).ToListAsync();
+        }
+
+        public async Task<Product> GetProductAsync(string Id)
+        {
+           return await _context.Products.Find(p => p.Id == Id).FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByNameAsync(string Name)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Name, Name);
+            return await _context.Products.Find(filter).ToListAsync();
+        }
+        public async Task<IEnumerable<Product>> GetProductByCategoryAsync(string Category)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Category, Category);
+            return await _context.Products.Find(filter).ToListAsync();
+        }
+
+        public async Task<bool> DeleteProductAsync(string Id)
+        {
+            if (string.IsNullOrEmpty(Id) || string.IsNullOrWhiteSpace(Id)) throw new ArgumentNullException("Id can\'t be null");
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Exists(p => p.Id == Id);
+
+            DeleteResult deleteResult = await _context.Products.DeleteOneAsync(filter);
+            return deleteResult.IsAcknowledged && deleteResult.DeletedCount > 0;
+        }
+
+        public async Task<bool> UpdateProductAsync(Product product)
+        {
+            var isUpdated = await _context
+                                     .Products
+                                     .ReplaceOneAsync(filter: p => p.Id == product.Id, replacement: product);
+            return isUpdated.IsAcknowledged
+                    && isUpdated.ModifiedCount > 0; 
+        }
+
+        public async Task CreateProduct(Product product)
+        {
+            if (product == null) throw new ArgumentNullException("Prodcut can\'t be null!");
+            await _context.Products.InsertOneAsync(product);
+        }
+    }
+}
