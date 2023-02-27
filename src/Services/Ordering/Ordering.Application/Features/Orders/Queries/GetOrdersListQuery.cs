72a909d19be8df77bd0c097f09ea3cbe809d8a71
KojoBoat <egyirchristian07@gmail.com>
2023-02-27 09:17:04 +0000

    Develop Application Layer, add first query feature following CQRS using MediatR

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersListQuery.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Queries
+{
+    public class GetOrdersListQuery : IRequest<List<OrdersVm>>
+    {
+        public string UserName { get; set; }
+        public GetOrdersListQuery(string userName)
+        {
+            UserName = userName;
+        }
+
+        // ToDO: Implement the IRequestHandler here
+
+        //public class GetOrdersListQueryHandler : IRequestHandler<GetOrdersListQuery, List<OrdersVm>>
+        //{
+        //    public Task<List<OrdersVm>> Handle(GetOrdersListQuery request, CancellationToken cancellationToken)
+        //    {
+        //        throw new NotImplementedException();
+        //    }
+        //}
+    }
+}
